5d0bdfd744f209c7baac43e68b850a5ad1b7d7cc
Jeff Cave <jcavejr@gmail.com>
2018-09-24 22:05:01 -0400

    added usage

--- src/FireworkLauncher.cpp
+++ src/FireworkLauncher.cpp
@@ -5,5 +5,6 @@ using namespace std;
 
 int main(void) {
     cout << "FireworkLauncher Version: " << FireworkLauncher_VERSION_MAJOR << "." << FireworkLauncher_VERSION_MINOR << endl;
+    cout << "Usage: 'FireworkLauncher'" << endl;
     return 0;
 }
